5f2e481bcc53fdd97b905cf1fbbffeed40361cf3
tenieceb <tabusenbark@gmail.com>
2025-11-08 17:57:44 -0700

    updated .prettierrc file to include the single quote rule

--- src/js/product.js
+++ src/js/product.js
@@ -1,17 +1,17 @@
-import { getLocalStorage, setLocalStorage } from "./utils.mjs";
-import ProductData from "./ProductData.mjs";
+import { getLocalStorage, setLocalStorage } from './utils.mjs';
+import ProductData from './ProductData.mjs';
 
-const dataSource = new ProductData("tents");
+const dataSource = new ProductData('tents');
 
 function addProductToCart(product) {
-  let cartItems = getLocalStorage("so-cart");
+  let cartItems = getLocalStorage('so-cart');
 
   if (!Array.isArray(cartItems)) {
     cartItems = [];
   }
 
   cartItems.push(product);
-  setLocalStorage("so-cart", cartItems);
+  setLocalStorage('so-cart', cartItems);
 }
 
 // add to cart button event handler
@@ -22,5 +22,5 @@ async function addToCartHandler(e) {
 
 // add listener to Add to Cart button
 document
-  .getElementById("addToCart")
-  .addEventListener("click", addToCartHandler);
+  .getElementById('addToCart')
+  .addEventListener('click', addToCartHandler);
