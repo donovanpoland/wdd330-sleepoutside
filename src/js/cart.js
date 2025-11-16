f28e0b4aa1ad2f27cad82e4ac9d12387a9ec84ea
tenieceb <tabusenbark@gmail.com>
2025-11-16 15:05:47 -0700

    updated to the point of having dynamic header/footer with dynamic product listing continuing to merge

--- src/js/cart.js
+++ src/js/cart.js
@@ -1,9 +1,11 @@
-import { getLocalStorage } from './utils.mjs';
+import { getLocalStorage, loadHeaderFooter } from "./utils.mjs";
+
+loadHeaderFooter();
 
 function renderCartContents() {
   // Get cart items from local storage, if none get an empty array
-  const cartItems = getLocalStorage('so-cart') || [];
-  const list = document.querySelector('.product-list');
+  const cartItems = getLocalStorage("so-cart") || [];
+  const list = document.querySelector(".product-list");
   // Check if cart is empty
   if (!cartItems.length) {
     list.innerHTML = `<li class="empty">Your cart is empty.</li>`;
@@ -11,24 +13,24 @@ function renderCartContents() {
   }
 
   const htmlItems = cartItems.map(cartItemTemplate);
-  list.innerHTML = htmlItems.join('');
+  list.innerHTML = htmlItems.join("");
 }
 
 function cartItemTemplate(item) {
   const qty = item.quantity ?? 1;
-  const newItem = `<li class='cart-card divider'>
-  <a href='#' class='cart-card__image'>
+  const newItem = `<li class="cart-card divider">
+  <a href="#" class="cart-card__image">
     <img
-      src='${item.Image}'
-      alt='${item.Name}'
+      src="${item.Image}"
+      alt="${item.Name}"
     />
   </a>
-  <a href='#'>
-    <h2 class='card__name'>${item.Name}</h2>
+  <a href="#">
+    <h2 class="card__name">${item.Name}</h2>
   </a>
-  <p class='cart-card__color'>${item.Colors[0].ColorName}</p>
-  <p class='cart-card__quantity'>qty: ${qty}</p>
-  <p class='cart-card__price'>$${(item.FinalPrice * qty).toFixed(2)}</p>
+  <p class="cart-card__color">${item.Colors[0].ColorName}</p>
+  <p class="cart-card__quantity">qty: ${qty}</p>
+  <p class="cart-card__price">$${(item.FinalPrice * qty).toFixed(2)}</p>
 </li>`;
 
   return newItem;
--- src/js/main.js
+++ src/js/main.js
@@ -1,10 +1,12 @@
-import ProductList from './ProductList.mjs';
-import ProductData from './ProductData.mjs';
+import ProductList from "./ProductList.mjs";
+import ProductData from "./ProductData.mjs";
+import { loadHeaderFooter } from "./utils.mjs";
 
-const dataSource = new ProductData('tents');
+const dataSource = new ProductData("tents");
 
-const listElement = document.querySelector('.product-list');
+const listElement = document.querySelector(".product-list");
 
-const productList = new ProductList('Tents', dataSource, listElement);
+const productList = new ProductList("Tents", dataSource, listElement);
 
 productList.init();
+loadHeaderFooter();
